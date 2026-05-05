bb9b66d7359856be4d07f3f5ba7faa100e0c0f96
gpmendes7 <andrewsseven@gmail.com>
2026-05-05 18:01:01 -0300

    commit

---
+++ src/main/java/br/com/gpmendes7/files/application/Program.java
@@ -0,0 +1,27 @@
+package br.com.gpmendes7.files.application;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Program {
+
+    public static void main(String[] args) {
+        File file = new File("src/main/resources/in7.txt");
+        Scanner sc = null;
+        try {
+            sc = new Scanner(file);
+            while (sc.hasNextLine()) {
+                System.out.println(sc.nextLine());
+            }
+        } catch (IOException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+        finally {
+            if(sc != null) {
+                sc.close();
+            }
+            sc.close();
+        }
+    }
+}
